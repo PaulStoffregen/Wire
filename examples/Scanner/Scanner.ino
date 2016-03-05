4d077115eca45a95840b10f1d5373854b5e586e5
PaulStoffregen <paul@pjrc.com>
2016-03-05 02:33:58 -0800

    Add more chips names to Scanner

--- examples/Scanner/Scanner.ino
+++ examples/Scanner/Scanner.ino
@@ -83,31 +83,43 @@ void loop() {
 
 void printKnownChips(byte address)
 {
+  // Is this list missing part numbers for chips you use?
+  // Please suggest additions here:
+  // https://github.com/PaulStoffregen/Wire/issues/new
   switch (address) {
+    case 0x00: Serial.print(F("AS3935")); break;
+    case 0x01: Serial.print(F("AS3935")); break;
+    case 0x02: Serial.print(F("AS3935")); break;
+    case 0x03: Serial.print(F("AS3935")); break;
     case 0x0A: Serial.print(F("SGTL5000")); break;
+    case 0x0B: Serial.print(F("SMBusBattery?")); break;
     case 0x0C: Serial.print(F("AK8963")); break;
     case 0x10: Serial.print(F("CS4272")); break;
     case 0x11: Serial.print(F("Si4713")); break;
     case 0x13: Serial.print(F("VCNL4000,AK4558")); break;
-    case 0x19: Serial.print(F("LSM303")); break;
+    case 0x18: Serial.print(F("LIS331DLH")); break;
+    case 0x19: Serial.print(F("LSM303,LIS331DLH")); break;
     case 0x1A: Serial.print(F("WM8731")); break;
-    case 0x1D: Serial.print(F("LSM9DS0,ADXL345,MMA7455L")); break;
-    case 0x1E: Serial.print(F("LSM303,HMC5883L,FXOS8700")); break;
-    case 0x20: Serial.print(F("MCP23017,FXAS21002")); break;
-    case 0x21: Serial.print(F("MCP23017")); break;
-    case 0x22: Serial.print(F("MCP23017")); break;
-    case 0x23: Serial.print(F("MCP23017")); break;
-    case 0x24: Serial.print(F("MCP23017")); break;
-    case 0x25: Serial.print(F("MCP23017")); break;
-    case 0x26: Serial.print(F("MCP23017")); break;
-    case 0x27: Serial.print(F("MCP23017")); break;
-    case 0x28: Serial.print(F("BNO055")); break;
-    case 0x29: Serial.print(F("TSL2561,VL6180")); break;
-    case 0x2A: Serial.print(F("SGTL5000")); break;
+    case 0x1C: Serial.print(F("LIS3MDL")); break;
+    case 0x1D: Serial.print(F("LSM303D,LSM9DS0,ADXL345,MMA7455L,LSM9DS1,LIS3DSH")); break;
+    case 0x1E: Serial.print(F("LSM303D,HMC5883L,FXOS8700,LIS3DSH")); break;
+    case 0x20: Serial.print(F("MCP23017,MCP23008,FXAS21002")); break;
+    case 0x21: Serial.print(F("MCP23017,MCP23008")); break;
+    case 0x22: Serial.print(F("MCP23017,MCP23008")); break;
+    case 0x23: Serial.print(F("MCP23017,MCP23008")); break;
+    case 0x24: Serial.print(F("MCP23017,MCP23008")); break;
+    case 0x25: Serial.print(F("MCP23017,MCP23008")); break;
+    case 0x26: Serial.print(F("MCP23017,MCP23008")); break;
+    case 0x27: Serial.print(F("MCP23017,MCP23008,LCD16x2,DigoleDisplay")); break;
+    case 0x28: Serial.print(F("BNO055,EM7180,CAP1188")); break;
+    case 0x29: Serial.print(F("TSL2561,VL6180,TSL2561,TSL2591,BNO055,CAP1188")); break;
+    case 0x2A: Serial.print(F("SGTL5000,CAP1188")); break;
+    case 0x2B: Serial.print(F("CAP1188")); break;
     case 0x38: Serial.print(F("RA8875,FT6206")); break;
-    case 0x3C: Serial.print(F("SSD1306")); break;
+    case 0x39: Serial.print(F("TSL2561")); break;
+    case 0x3C: Serial.print(F("SSD1306,DigisparkOLED")); break;
     case 0x3D: Serial.print(F("SSD1306")); break;
-    case 0x40: Serial.print(F("PCA9685")); break;
+    case 0x40: Serial.print(F("PCA9685,Si7021")); break;
     case 0x41: Serial.print(F("STMPE610,PCA9685")); break;
     case 0x42: Serial.print(F("PCA9685")); break;
     case 0x43: Serial.print(F("PCA9685")); break;
@@ -115,8 +127,8 @@ void printKnownChips(byte address)
     case 0x45: Serial.print(F("PCA9685")); break;
     case 0x46: Serial.print(F("PCA9685")); break;
     case 0x47: Serial.print(F("PCA9685")); break;
-    case 0x48: Serial.print(F("ADS1115,PN532,TMP102")); break;
-    case 0x49: Serial.print(F("ADS1115")); break;
+    case 0x48: Serial.print(F("ADS1115,PN532,TMP102,PCF8591")); break;
+    case 0x49: Serial.print(F("ADS1115,TSL2561,PCF8591")); break;
     case 0x4A: Serial.print(F("ADS1115")); break;
     case 0x4B: Serial.print(F("ADS1115,TMP102")); break;
     case 0x50: Serial.print(F("EEPROM")); break;
@@ -127,15 +139,22 @@ void printKnownChips(byte address)
     case 0x55: Serial.print(F("EEPROM")); break;
     case 0x56: Serial.print(F("EEPROM")); break;
     case 0x57: Serial.print(F("EEPROM")); break;
-    case 0x58: Serial.print(F("TPA2016")); break;
+    case 0x58: Serial.print(F("TPA2016,MAX21100")); break;
     case 0x5A: Serial.print(F("MPR121")); break;
     case 0x60: Serial.print(F("MPL3115,MCP4725,MCP4728,TEA5767,Si5351")); break;
     case 0x61: Serial.print(F("MCP4725")); break;
-    case 0x62: Serial.print(F("LidarLite")); break;
-    case 0x68: Serial.print(F("MPU9250,ITG3200,DS1307,DS3231")); break;
+    case 0x62: Serial.print(F("LidarLite,MCP4725")); break;
+    case 0x63: Serial.print(F("MCP4725")); break;
+    case 0x68: Serial.print(F("DS1307,DS3231,MPU6050,MPU9050,MPU9250,ITG3200,ITG3701,LSM9DS0,L3G4200D")); break;
+    case 0x69: Serial.print(F("MPU6050,MPU9050,MPU9250,ITG3701,L3G4200D")); break;
+    case 0x6A: Serial.print(F("LSM9DS1")); break;
     case 0x6B: Serial.print(F("LSM9DS0")); break;
-    case 0x76: Serial.print(F("MS5607,MS5611")); break;
-    case 0x77: Serial.print(F("BMP085,BMA180")); break;
+    case 0x70: Serial.print(F("AdafruitLED")); break;
+    case 0x71: Serial.print(F("SFE7SEG,AdafruitLED")); break;
+    case 0x72: Serial.print(F("AdafruitLED")); break;
+    case 0x73: Serial.print(F("AdafruitLED")); break;
+    case 0x76: Serial.print(F("MS5607,MS5611,MS5637,BMP280")); break;
+    case 0x77: Serial.print(F("BMP085,BMA180,BMP280,MS5611")); break;
     default: Serial.print(F("unknown chip"));
   }
 }
