f72e03f34a3fa2f53ae22e62aa8a6b3bea66dbd0
PaulStoffregen <paul@pjrc.com>
2019-12-30 11:40:18 -0800

    Add BNO080 to list of known chips in Scanner example

--- examples/Scanner/Scanner.ino
+++ examples/Scanner/Scanner.ino
@@ -140,7 +140,7 @@ void printKnownChips(byte address)
     case 0x48: Serial.print(F("ADS1115,PN532,TMP102,LM75,PCF8591")); break;
     case 0x49: Serial.print(F("ADS1115,TSL2561,PCF8591")); break;
     case 0x4A: Serial.print(F("ADS1115")); break;
-    case 0x4B: Serial.print(F("ADS1115,TMP102")); break;
+    case 0x4B: Serial.print(F("ADS1115,TMP102,BNO080")); break;
     case 0x50: Serial.print(F("EEPROM")); break;
     case 0x51: Serial.print(F("EEPROM")); break;
     case 0x52: Serial.print(F("Nunchuk,EEPROM")); break;
