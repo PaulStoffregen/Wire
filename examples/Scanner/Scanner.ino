a9457dac399bb66857c429ef34c37edde7ee9ca3
PaulStoffregen <paul@pjrc.com>
2020-05-31 05:09:10 -0700

    Add Si7210 to Scanner example fixes #22

--- examples/Scanner/Scanner.ino
+++ examples/Scanner/Scanner.ino
@@ -122,7 +122,10 @@ void printKnownChips(byte address)
     case 0x2D: Serial.print(F("MCP44XX ePot")); break;
     case 0x2E: Serial.print(F("MCP44XX ePot")); break;
     case 0x2F: Serial.print(F("MCP44XX ePot")); break;
-    case 0x33: Serial.print(F("MAX11614,MAX11615")); break;
+    case 0x30: Serial.print(F("Si7210")); break;
+    case 0x31: Serial.print(F("Si7210")); break;
+    case 0x32: Serial.print(F("Si7210")); break;
+    case 0x33: Serial.print(F("MAX11614,MAX11615,Si7210")); break;
     case 0x34: Serial.print(F("MAX11612,MAX11613")); break;
     case 0x35: Serial.print(F("MAX11616,MAX11617")); break;
     case 0x38: Serial.print(F("RA8875,FT6206,MAX98390")); break;
